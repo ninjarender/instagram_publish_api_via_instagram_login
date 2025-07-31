f8ead94d62dbe049e00691ae4a84afbffbdc5e35
Vadym Kruchyna <kruchina.vadim@gmail.com>
2025-07-31 22:04:43 +0200

    init

---
+++ lib/instagram_publish_api_via_instagram_login.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "instagram_publish_api_via_instagram_login/version"
+
+module InstagramPublishApiViaInstagramLogin
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/instagram_publish_api_via_instagram_login_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe InstagramPublishApiViaInstagramLogin do
+  it "has a version number" do
+    expect(InstagramPublishApiViaInstagramLogin::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
